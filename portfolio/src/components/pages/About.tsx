18d17013c7eeb98bda562807bb3eeb9fc147733c
seolynnE <tjfls2004@gmail.com>
2023-12-14 12:02:25 +0900

    내용수정

--- portfolio/src/components/pages/About.tsx
+++ portfolio/src/components/pages/About.tsx
@@ -113,7 +113,7 @@ function Introduce() {
       <h3>소개</h3>
       <p className="title">정서린 SEO-LYNN JEONG</p>
       <p className="content">
-        저는 2년 차 웹퍼블리셔(마크업 개발자) 입니다. <br />
+        저는 3년 차 웹퍼블리셔 입니다. <br />
         단단한 구조를 설계해 감각적인 UI를 입혀 유지 보수에 최적화된 코드를
         그려냅니다.
         <br />웹 표준, 웹 접근성에 관심이 있으며 더욱 탄탄하고 역동적인 UI를
